4ce40efe5c50bb8d507a935e275fd38935331f7a
asquik <oleksiy.savytskyy@gmail.com>
2025-01-25 16:56:36 -0500

    debug input

---
+++ Assets/InputDebugScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class InputDebugScript : MonoBehaviour
+{
+    PlayerInput input;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var input = new PlayerInput();
+        var smth = input.Player.Get();
+        smth.actionTriggered += (x) => Debug.Log(x);
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+
+
+}
