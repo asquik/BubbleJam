4f1fbbf9d7cb531880a3dea6be75610552e6f3ab
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-01-26 16:36:17 -0500

    Implemented show/hide for power ups

--- Assets/ConfigurablePowerUp.cs
+++ Assets/ConfigurablePowerUp.cs
@@ -4,28 +4,34 @@ using System.Collections;
 public class ConfigurablePowerUp : MonoBehaviour
 {
     public PowerUpScriptableObject data;
+    public SpriteRenderer spriteRenderer;
+    public Collider2D collider;
 
     public void Start()
     {
-        var sRenderer = GetComponent<SpriteRenderer>();
-        sRenderer.sprite = data.sprite;
+        collider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = data.sprite;
+    }
+
+    private void toggleActive(bool isActive)
+    {
+        spriteRenderer.enabled = isActive;
+        collider.enabled = isActive;
     }
     
-    // private IEnumerator tempHide()
-    // {
-    //     gameObject.SetActive(false);
-    //     yield return new WaitForSeconds(3f);
-    //     gameObject.SetActive(true);
-    // }
+    private IEnumerator tempHide()
+    {
+        toggleActive(false);
+        yield return new WaitForSeconds(3f);
+        toggleActive(true);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         var activator = other.gameObject.GetComponent<PlayerAbilityActivator>();
         activator.ApplyAbility(data);
 
-        // StartCoroutine(tempHide());
-
-        // TBD how what the happy path will be
-        // GameObject.Destroy(gameObject);
+        StartCoroutine(tempHide());
     }
 }
