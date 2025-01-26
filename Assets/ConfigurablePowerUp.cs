fde83605b8f500138e99d6d4d77502eab4d23c7f
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-01-26 16:28:18 -0500

    Fixed bugs caused by code merging

--- Assets/ConfigurablePowerUp.cs
+++ Assets/ConfigurablePowerUp.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class ConfigurablePowerUp : MonoBehaviour
 {
@@ -9,12 +10,21 @@ public class ConfigurablePowerUp : MonoBehaviour
         var sRenderer = GetComponent<SpriteRenderer>();
         sRenderer.sprite = data.sprite;
     }
+    
+    // private IEnumerator tempHide()
+    // {
+    //     gameObject.SetActive(false);
+    //     yield return new WaitForSeconds(3f);
+    //     gameObject.SetActive(true);
+    // }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         var activator = other.gameObject.GetComponent<PlayerAbilityActivator>();
         activator.ApplyAbility(data);
-        
+
+        // StartCoroutine(tempHide());
+
         // TBD how what the happy path will be
         // GameObject.Destroy(gameObject);
     }
