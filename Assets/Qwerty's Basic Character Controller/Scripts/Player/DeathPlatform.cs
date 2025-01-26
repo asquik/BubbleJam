64e64c21632fec6fd67edfb58c48c757022ad571
JustinBordage <38659460+JustinBordage@users.noreply.github.com>
2025-01-25 20:15:07 -0500

    Finished implementation of the death platform

--- Assets/Qwerty's Basic Character Controller/Scripts/Player/DeathPlatform.cs
+++ Assets/Qwerty's Basic Character Controller/Scripts/Player/DeathPlatform.cs
@@ -4,43 +4,8 @@ public class DeathPlatform : MonoBehaviour
 {
     [SerializeField] private Transform respawnPoint;
     
-    private Transform abyssPlatform;
-    private LayerMask playerLayer;
-    
-    public void Start()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        abyssPlatform = transform.Find("Abyss").transform;
-        playerLayer = LayerMask.GetMask("Player");
-    }
-    
-    // public void Update()
-    // {
-    //     if (abyssCheck())
-    //     {
-    //         respawn();
-    //     }
-    // }
-
-    void respawn()
-    {
-        
-        Debug.Log("Respawn Player");
-    }
-    
-    bool abyssCheck()
-    {
-        return Physics2D.OverlapBox(abyssPlatform.position, new Vector2(0.2f, 0.2f), playerLayer);
-    }
-
-    public void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collision Enter");
-        Debug.Log(collision.gameObject);
-    }
-    
-    public void OnTriggerEnter(Collider collision)
-    {
-        Debug.Log("Tigger Enter");
-        Debug.Log(collision.gameObject);
+		collision.gameObject.transform.position = respawnPoint.position;
     }
 }
